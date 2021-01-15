5a5d58f5cf1b01ae157ff2322ece664cad086185
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2021-01-15 19:23:27 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -29,7 +29,7 @@ from hachoir.parser import createParser
 from PIL import Image
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["c2v"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["rename_vid"]))
 async def convert_to_video(bot, update):
     if update.from_user.id in Config.BANNED_USERS:
         await bot.send_message(
@@ -38,7 +38,7 @@ async def convert_to_video(bot, update):
             reply_to_message_id=update.message_id
         )
         return
-    TRChatBase(update.from_user.id, update.text, "c2v")
+    TRChatBase(update.from_user.id, update.text, "rename_vid")
     if update.reply_to_message is not None:
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
